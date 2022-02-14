30900d1d3d26448ecd34d520a45eebf649c2dfa6
Gustavo Ferreira Fogolin <fgfogolin@hotmail.com>
2022-02-13 23:22:56 -0300

    Added all CRUD views

--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,27 @@
 from django.http.response import Http404
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 
 # Create your views here.
+from .forms import NotesForm
 from .models import Notes
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesListView(ListView):
     model = Notes
