ea12b956458f5550ef65e1f88a9b0b782060a071
Gustavo Ferreira Fogolin <fgfogolin@hotmail.com>
2022-02-14 22:01:24 -0300

    End of the course, smart notes ready

--- notes/views.py
+++ notes/views.py
@@ -1,36 +1,51 @@
 from django.http.response import Http404
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 from .forms import NotesForm
 from .models import Notes
 
-class NotesDeleteView(DeleteView):
+class NotesDeleteView(LoginRequiredMixin, DeleteView):
     model = Notes
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html'
+    login_url = '/admin'
 
-class NotesUpdateView(UpdateView):
+class NotesUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = '/admin'
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = '/admin'
 
-class NotesListView(ListView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = '/admin'
 
-class NotesDetailView(DetailView):
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+class NotesDetailView(LoginRequiredMixin, DetailView):
     model = Notes
     context_object_name = "note"
+    login_url = '/admin'
 
 
 def detail(request, pk):
